22efae88c951e19de3647cb098d1cb0ee138e903
Big Boss Linux <klemidz@gmail.com>
2019-03-17 06:25:21 +0100

    timestamp project done

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const http = require('http');
 const fs = require('fs');
-const path = require('path');
+const url = require('url');
 
 const PORT = process.env.PORT || 1337;
 
+
+ 
 const server = http.createServer((req,res)=>{
-  console.log(new Date());
+
+  
+  if(req.url == "/"){
+    const date = new Date();
+    res.setHeader("200", {'content-type' : "application/json"});
+    const json = {unix: date.getTime(), utc: date.toUTCString()};
+    res.end(JSON.stringify(json));
+  }else if(req.url.slice(1).match(/^\d+$/)){
+      const date = new Date(parseInt(req.url.slice(1)));
+      res.setHeader("200", {'content-type' : "application/json"});      
+      const json = {unix: date.getTime(), utc: date.toUTCString()};
+      res.end(JSON.stringify(json));
+  }else{
+    const date = new Date(req.url.slice(1));
+    if (date != "Invalid Date"){
+      res.setHeader("200", {'content-type' : "application/json"});      
+      const json = {unix: date.getTime(), utc: date.toUTCString()};
+      res.end(JSON.stringify(json));
+    }else{
+      const json = {"error" : "Invalid Date" };
+      res.setHeader("200", {'content-type' : "application/json"});
+      res.end(JSON.stringify(json));
+    }
+  }
 });
-
-
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
